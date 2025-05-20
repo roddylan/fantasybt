903ccebc8e82cccaa9c31ce48d398a8eeebac59a
Roderick Lan <rodericklan03@gmail.com>
2025-05-20 16:41:38 -0600

    arb strat interface

--- include/arb_strat.hpp
+++ include/arb_strat.hpp
@@ -2,12 +2,20 @@
 // simple strategy
 #ifndef ARB_STRAT_HPP_
 #define ARB_STRAT_HPP_
+#include "strategy.hpp"
 #include <functional>
 
 
-namespace ARB_STRAT {
-  
-}
+class ArbStrategy : public Strategy {
+public:
+  ~ArbStrategy();
+
+  void draft_strategy(const BasePlayer<T_FOOTBALL> &, const int &) override;
+  void roster_strategy() override;
+  void trade_strategy(const BasePlayer<T_FOOTBALL> &p1, const BasePlayer<T_FOOTBALL> &p2) override;
+
+private:
+};
 
 
 

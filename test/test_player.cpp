5085014992fdc0044e68a2cde00cd14c24fd9ba9
Roderick Lan <rodericklan03@gmail.com>
2025-05-22 13:42:09 -0600

    test stuff

--- test/test_player.cpp
+++ test/test_player.cpp
@@ -6,9 +6,15 @@ int main() {
   BasePlayer<Position::Football> temp{
       "CeeDee Lamb", 1, std::vector<Position::Football>{Position::Football::WR},
       PlayerStatus::H};
+  std::cout << temp << std::endl;
+  std::string base{"CeeDee Lamb | WR | Cur Rank: 1.00 | Status: H"};
+  assert(base == temp.to_string());
+  assert(base == std::string{temp});
+  
+  assert(temp.get_status() == PlayerStatus::H);
+  temp.set_status(PlayerStatus::DTDO);
+  assert(temp.get_status() == PlayerStatus::DTDO);
 
-  // std::cout << temp.to_string();
-  std::cout << temp;
 
   return 0;
 }

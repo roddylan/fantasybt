fa1b24922f55025b116b785690421701854acafb
Roderick Lan <rodericklan03@gmail.com>
2025-05-22 13:33:20 -0600

    assignment and copy op

--- include/roster.hpp
+++ include/roster.hpp
@@ -1,6 +1,7 @@
 #ifndef ROSTER_HPP_
 #define ROSTER_HPP_
 #include "player.hpp"
+#include <algorithm>
 #include <unordered_map>
 #include <vector>
 #include <memory>
@@ -9,6 +10,10 @@ template <typename T> class AvailablePlayers {};
 
 template <typename T> struct Roster {
   Roster() = delete;
+  Roster(const Roster &roster) {
+    avail_pos = std::copy(roster.avail_pos);
+    players = std::copy(roster.players);
+  }
   Roster(const std::unordered_map<T, size_t> &_avail_pos,
          const std::vector<T> &_players)
       : avail_pos{_avail_pos}, players{_players} {}
@@ -18,6 +23,14 @@ template <typename T> struct Roster {
       : avail_pos{_avail_pos}, players(_sz) {}
   //
 
+
+  Roster &operator=(const Roster &rhs) {
+    if (&rhs == this) { return *this; }
+    this->avail_pos = rhs.avail_pos;
+    this->players = rhs.players;
+    return *this;
+  }
+  
   std::unordered_map<T, size_t> avail_pos;
   std::vector<std::shared_ptr<BasePlayer<T>>> players;
 };

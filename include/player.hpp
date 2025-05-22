c538c705bad0f7a70ab4820c2623fb667fba51f5
Roderick Lan <rodericklan03@gmail.com>
2025-05-22 14:46:48 -0600

    ao

--- include/player.hpp
+++ include/player.hpp
@@ -35,18 +35,28 @@ enum class PlayerStatus {
   Q,    // questionable
   DTDO, // dtd or out
   IR,   // injured
+  DNE,  // does not exist
 };
 
 // TODO: type constraint
 template <typename T> class BasePlayer {
 public:
   // TODO: add projected rank
+  BasePlayer() : name{"None"}, rank{-1}, pos{}, status{PlayerStatus::DNE} {}
   BasePlayer(const std::string &_name, const float &_rank,
              const std::vector<T> &_pos, const PlayerStatus &_status)
       : name{_name}, rank{_rank}, pos{_pos}, status{_status} {}
   //
-  ~BasePlayer() {};
+  BasePlayer(const BasePlayer &p) : name{p.name}, rank{p.rank}, pos{p.pos}, status{p.status} {}
+  ~BasePlayer(){};
 
+  BasePlayer &operator=(const BasePlayer &rhs) {
+    if (*rhs == this) { return *this; }
+    name = rhs.name;
+    rank = rhs.rank;
+    pos = rhs.pos;
+    status = rhs.status;
+  }
   std::vector<T> get_pos() const { return pos; }
 
   PlayerStatus get_status() const { return status; }
@@ -58,7 +68,7 @@ public:
     for (const auto &p : pos) {
       positions += std::string(magic_enum::enum_name(p)) + " ";
     }
-    
+
     return fmt::format("{} | {}| Cur Rank: {:.2f} | Status: {}", name,
                        positions, rank, magic_enum::enum_name(status));
   }
@@ -77,7 +87,7 @@ private:
   PlayerStatus status;
 };
 
-template<typename T>
+template <typename T>
 std::ostream &operator<<(std::ostream &os, const BasePlayer<T> &player) {
   os << player.to_string();
   return os;

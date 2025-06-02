3ab531a2d356644ca0f7d2c791af7f7e6386750b
Roderick Lan <rodericklan03@gmail.com>
2025-06-02 16:25:59 -0600

    const and noexcept + todo

--- include/state.hpp
+++ include/state.hpp
@@ -12,42 +12,56 @@ public:
   TeamState() : roster{}, total_pts{} {}
   // value construct
   TeamState(const std::vector<std::shared_ptr<BasePlayer<SPORT>>> &_roster,
-            const size_t &_total_pts)
+            const long &_total_pts)
       : roster(_roster), total_pts(_total_pts) {}
   // copy construct
   TeamState(const TeamState<SPORT> &other) : roster(other.roster), total_pts{other.total_pts} {}
   // move construct
   TeamState(TeamState<SPORT> &&other) : roster(std::move(other.roster)), total_pts{other.total_pts} {}
   // copy assign
-  TeamState& operator=(const TeamState<SPORT> &other) {
+  TeamState<SPORT>& operator=(const TeamState<SPORT> &other) {
     if (&other == this) { return *this; }
     this->total_pts = other.total_pts;
     this->roster = other.roster;
     return *this;
   }
   // move assign
-  TeamState& operator=(TeamState<SPORT> &&other) {
+  TeamState<SPORT>& operator=(TeamState<SPORT> &&other) {
     if (&other == this) { return *this; }
     this->total_pts = other.total_pts;
     this->roster = std::move(other.roster);
     return *this;
   }
 
-  bool operator<(TeamState<SPORT> &other) {
+  bool operator<(const TeamState<SPORT> &other) const noexcept {
     return this->total_pts < other.total_pts;
   }
   
-  bool operator>(TeamState<SPORT> &other) {
+  bool operator>(const TeamState<SPORT> &other) const noexcept {
     return this->total_pts > other.total_pts;
   }
   
-  bool operator==(TeamState<SPORT> &other) {
+  bool operator==(const TeamState<SPORT> &other) const noexcept {
     return this->total_pts == other.total_pts;
   }
+  // TODO addition ops
+  void set_total_pts(const long &pts) {
+    total_pts = pts;
+  }
 
+  long get_total_pts() {
+    return total_pts;
+  }
+
+  long update_total_pts(const long &add_pts) {
+    total_pts += add_pts;
+    return total_pts;
+  }
+
+  
 private:
   std::vector<std::shared_ptr<BasePlayer<SPORT>>> roster;
-  size_t total_pts;
+  long total_pts;
 };
 // hold roster, total pts
 
